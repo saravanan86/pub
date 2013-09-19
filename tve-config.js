acd134ff3296d63b33696e13d5c94f159c0ffbef
Saravanan <saravanan86@gmail.com>
2013-09-19 16:32:43 -0400

    BTGREPGEN-3244
    TVE test rig config

---
+++ tve-config.js
@@ -0,0 +1,20 @@
+[
+	{
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "Bento TestRig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"TVERequestorID" : "NICK",
+		"TVEChannelID" : "NICK",
+		"TVEBrand" : "nick",
+		"TVEEnvironment" : "development"
+	}
+]
