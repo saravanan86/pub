65531d7af36adb73e1c9e9d04be11bee063bb78a
saravanan86 <saravanan86@gmail.com>
2014-02-18 15:41:07 -0500

    Create app-config.js
    Test config

---
+++ app-config.js
@@ -0,0 +1,18 @@
+[
+	{
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "Bento TestRig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : "2",
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true"
+	}
+]
