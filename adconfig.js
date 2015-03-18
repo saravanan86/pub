8f4f22474cc40ce6317385bae0e6e66b8a5fc799
saravanan86 <saravanan86@gmail.com>
2015-03-18 17:12:11 -0400

    adconfig.js
    adconfig.js for testing Sizmek Ads

---
+++ adconfig.js
@@ -0,0 +1,40 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : false,
+			"audienceManagerEnabled": true
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 250,
+			"appName" : "nick_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : false,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : false,
+			"refreshInterval" : 10,
+			"enableDefaultBumper" : false,
+			"enableCoppaAdBanner" : false,
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmode" : "on"
+			},
+			"dfpData" : {
+				"metadata" : {
+					"property" : "nick",
+					"hub" : "breaking",
+					"is_main" : true
+				},
+				"mappings" : {
+					"ad-unit" : {
+						"1" : "@property"
+					}
+				}		        	
+			}
+		}
+	}
+]
